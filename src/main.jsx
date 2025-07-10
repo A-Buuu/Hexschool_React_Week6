fef6da7df4d7d88cc881185f2a9cd1b5da6df20e
abuchiang <abuuu.chiang@gmail.com>
2025-07-10 13:35:42 +0800

    建立 router 與撰寫路由表

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // 引入 css
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { RouterProvider } from "react-router-dom";
 
-import App from './App.jsx'
+// import App from './App.jsx'
+import router from './router'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
